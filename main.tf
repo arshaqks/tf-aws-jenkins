a270671b96630a1fe27c1c932420182126e4bd84
Arshaq K S <130451726+arshaqks@users.noreply.github.com>
2023-10-11 17:58:22 +0530

    Update main.tf with user_data argument

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "public_instance" {
  ami           = var.ami
  instance_type = var.instance_type
+ user_data     = file("./jenkins.sh")
 
  tags = {
    Name = var.name_tag,
