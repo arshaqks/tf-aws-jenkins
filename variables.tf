ef4bf9505fd7f9f4ee164ccd12c70cca4dfa0763
arshaqks <ks.arshaq@gmail.com>
2023-10-13 18:24:34 +0530

    removed access and secret variable block

--- variables.tf
+++ variables.tf
@@ -1,12 +1,4 @@
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-}
 
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-}
 
 variable "aws_region" {
   description = "AWS region"
